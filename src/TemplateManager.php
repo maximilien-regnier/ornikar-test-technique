bd9faeba3662fe68cb2f12c4d97f917a814daab0
maximilien regnier <regnier.maximilien@gmail.com>
2022-08-11 12:41:50 +0200

    Throw exception if lesson isn't defined

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -10,6 +10,7 @@ use App\Entity\Template;
 use App\Repository\InstructorRepository;
 use App\Repository\LessonRepository;
 use App\Repository\MeetingPointRepository;
+use Exception;
 
 class TemplateManager
 {
@@ -30,9 +31,11 @@ class TemplateManager
     {
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
 
-        $lesson = $this->isLessonDefined($data) ? $data['lesson'] : null;
+        try {
+            $this->isLessonDefined($data);
 
-        if ($lesson) {
+            $lesson = $data['lesson'];
+
             $_lessonFromRepository = LessonRepository::getInstance()->getById($lesson->id);
             $usefulObject = MeetingPointRepository::getInstance()->getById($lesson->meetingPointId);
             $instructorOfLesson = InstructorRepository::getInstance()->getById($lesson->instructorId);
@@ -60,42 +63,53 @@ class TemplateManager
             }
 
             (strpos($text, '[lesson:instructor_name]') !== false) and $text = str_replace('[lesson:instructor_name]', $instructorOfLesson->firstname, $text);
+
+
+            if ($lesson->meetingPointId) {
+                if (strpos($text, '[lesson:meeting_point]') !== false)
+                    $text = str_replace('[lesson:meeting_point]', $usefulObject->name, $text);
+            }
+
+            if (strpos($text, '[lesson:start_date]') !== false)
+                $text = str_replace('[lesson:start_date]', $lesson->start_time->format('d/m/Y'), $text);
+
+            if (strpos($text, '[lesson:start_time]') !== false)
+                $text = str_replace('[lesson:start_time]', $lesson->start_time->format('H:i'), $text);
+
+            if (strpos($text, '[lesson:end_time]') !== false)
+                $text = str_replace('[lesson:end_time]', $lesson->end_time->format('H:i'), $text);
+
+
+            if (isset($data['instructor']) and ($data['instructor'] instanceof Instructor))
+                $text = str_replace('[instructor_link]', 'instructors/' . $data['instructor']->id . '-' . urlencode($data['instructor']->firstname), $text);
+            else
+                $text = str_replace('[instructor_link]', '', $text);
+
+            /*
+             * USER
+             * [user:*]
+             */
+            $_user = (isset($data['user']) and ($data['user'] instanceof Learner)) ? $data['user'] : $APPLICATION_CONTEXT->getCurrentUser();
+            if ($_user) {
+                (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]', ucfirst(strtolower($_user->firstname)), $text);
+            }
+
+        } catch (Exception $exception) {
+
         }
 
-        if ($lesson->meetingPointId) {
-            if (strpos($text, '[lesson:meeting_point]') !== false)
-                $text = str_replace('[lesson:meeting_point]', $usefulObject->name, $text);
-        }
 
-        if (strpos($text, '[lesson:start_date]') !== false)
-            $text = str_replace('[lesson:start_date]', $lesson->start_time->format('d/m/Y'), $text);
-
-        if (strpos($text, '[lesson:start_time]') !== false)
-            $text = str_replace('[lesson:start_time]', $lesson->start_time->format('H:i'), $text);
-
-        if (strpos($text, '[lesson:end_time]') !== false)
-            $text = str_replace('[lesson:end_time]', $lesson->end_time->format('H:i'), $text);
-
-
-        if (isset($data['instructor']) and ($data['instructor'] instanceof Instructor))
-            $text = str_replace('[instructor_link]', 'instructors/' . $data['instructor']->id . '-' . urlencode($data['instructor']->firstname), $text);
-        else
-            $text = str_replace('[instructor_link]', '', $text);
-
-        /*
-         * USER
-         * [user:*]
-         */
-        $_user = (isset($data['user']) and ($data['user'] instanceof Learner)) ? $data['user'] : $APPLICATION_CONTEXT->getCurrentUser();
-        if ($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]', ucfirst(strtolower($_user->firstname)), $text);
-        }
 
         return $text;
     }
 
-    private function isLessonDefined($data): bool
+    /**
+     * @throws Exception
+     */
+    private function isLessonDefined($data)
     {
-        return (isset($data['lesson']) and $data['lesson'] instanceof Lesson);
+        if (!(isset($data['lesson']) and $data['lesson'] instanceof Lesson)) {
+            throw new Exception('Lesson not found in given data');
+        }
     }
 }
